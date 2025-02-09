611e929b02c795edfd507204fc17e8380e8def9e
U-KIMKINGKONG\Pakorn <pjantacumma@wisc.edu>
2025-02-09 15:01:54 -0600

    modify image.c

--- homework/common/black_jack/image.c
+++ homework/common/black_jack/image.c
@@ -1,6 +1,6 @@
 /**
  * @file image.c
- * @author <ADD NAME>
+ * @author Pakorn Jantacumma
  * @brief 
  * @version 0.1
  * @date 2025-01-22
@@ -34,5 +34,68 @@
  */
  bool image_get_info(image_type_t img_type, image_t *image)
  {
+
+   switch (img_type)
+   {
+   // HEART CASE
+   case IMAGE_TYPE_HEART:
+      image->width = IMAGE_PIXELS_WIDTH_HEART;
+      image->height = IMAGE_PIXELS_HEIGHT_HEART;
+      image->fcolor = 0xF800; // red color
+      image->bcolor = 0xFFFF; // white
+      image->bitmap = IMAGE_BITMAP_HEART;
+      break;
+   
+   // DIAMOND CASE
+   case IMAGE_TYPE_DIAMOND:
+      image->width = IMAGE_PIXELS_WIDTH_DIAMOND;
+      image->height = IMAGE_PIXELS_HEIGHT_DIAMOND;
+      image->fcolor = 0xF800; // red color
+      image->bcolor = 0xFFFF; // white
+      image->bitmap = IMAGE_BITMAP_DIAMOND;
+      break;
+   
+   // CLUB CASE
+   case IMAGE_TYPE_CLUB:
+      image->width = IMAGE_PIXELS_WIDTH_CLUB;
+      image->height = IMAGE_PIXELS_HEIGHT_CLUB;
+      image->fcolor = 0x0000; // black color
+      image->bcolor = 0xFFFF; // white
+      image->bitmap = IMAGE_BITMAP_CLUB;
+      break;
+   
+   // SPADE CASE
+   case IMAGE_TYPE_SPADE:
+      image->width = IMAGE_PIXELS_WIDTH_SPADE;
+      image->height = IMAGE_PIXELS_HEIGHT_SPADE;
+      image->fcolor = 0x0000; // black color
+      image->bcolor = 0xFFFF; // white
+      image->bitmap = IMAGE_BITMAP_SPADE;
+      break;
+      
+   // START SCREEN CASE
+   case IMAGE_TYPE_START_SCREEN:
+      image->width = IMAGE_PIXELS_WIDTH_START_SCREEN;
+      image->height = IMAGE_PIXELS_HEIGHT_START_SCREEN;
+      image->fcolor = 0x001F; // blue color
+      image->bcolor = 0x0000; // black
+      image->bitmap = IMAGE_BITMAP_START_SCREEN;
+      break;
+   
+    // BET SCREEN CASE
+   case IMAGE_TYPE_BET_SCREEN:
+      image->width = IMAGE_PIXELS_WIDTH_BET_SCREEN;
+      image->height = IMAGE_PIXELS_HEIGHT_BET_SCREEN;
+      image->fcolor = 0x001F; // blue color
+      image->bcolor = 0x0000; // black
+      image->bitmap = IMAGE_BITMAP_START_SCREEN;
+      break;
+
+   // return false if not into any category, or font
+   default:
+      return false;
+      break;
+   }
+
     return true;
  }

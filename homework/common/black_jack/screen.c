e7ee4ea7df54e95b436080658629a31db5d6e092
U-ENGR\dhamdhere <dhamdhere@win-8002.ad.engr.wisc.edu>
2025-02-11 16:49:36 -0600

    HW01 Submission

--- homework/common/black_jack/screen.c
+++ homework/common/black_jack/screen.c
@@ -19,94 +19,88 @@
 void screen_display_stats_funds(int16_t funds, uint16_t fcolor)
 {
     // initialize all associate character
-    image_t *F = (image_t *)malloc(sizeof(image_t));
-    image_t *U = (image_t *)malloc(sizeof(image_t));
-    image_t *N = (image_t *)malloc(sizeof(image_t));
-    image_t *D = (image_t *)malloc(sizeof(image_t));
-    image_t *S = (image_t *)malloc(sizeof(image_t));
-    image_t *colon = (image_t *)malloc(sizeof(image_t));
+    image_t F;
+    image_t U;
+    image_t N;
+    image_t D;
+    image_t S;
+    image_t colon;
 
     // get character image
-    font_get_image('F', F);
-    font_get_image('U', U);
-    font_get_image('N', N);
-    font_get_image('D', D);
-    font_get_image('S', S);
-    font_get_image(':', colon);
+    font_get_image('F', &F);
+    font_get_image('U', &U);
+    font_get_image('N', &N);
+    font_get_image('D', &D);
+    font_get_image('S', &S);
+    font_get_image(':', &colon);
 
      // Turn LCD On F
      lcd_draw_image(
         LOC_GAME_LABEL_FUNDS_X,
-        LOC_GAME_LABEL_FUNDS_Y,
-        F->width,
-        F->height,
-        F->bitmap,
+        LOC_GAME_LABEL_FUNDS_Y - 10,
+        F.width,
+        F.height,
+        F.bitmap,
         fcolor,
-        F->bcolor,
+        F.bcolor,
         false);
 
         // Turn LCD On U
      lcd_draw_image(
         LOC_GAME_LABEL_FUNDS_X + 15,
-        LOC_GAME_LABEL_FUNDS_Y,
-        U->width,
-        U->height,
-        U->bitmap,
+        LOC_GAME_LABEL_FUNDS_Y - 10,
+        U.width,
+        U.height,
+        U.bitmap,
         fcolor,
-        U->bcolor,
+        U.bcolor,
         false);
 
         // Turn LCD On N
      lcd_draw_image(
         LOC_GAME_LABEL_FUNDS_X + 30,
-        LOC_GAME_LABEL_FUNDS_Y,
-        N->width,
-        N->height,
-        N->bitmap,
+        LOC_GAME_LABEL_FUNDS_Y - 10,
+        N.width,
+        N.height,
+        N.bitmap,
         fcolor,
-        N->bcolor,
+        N.bcolor,
         false);
 
         // Turn LCD On D
      lcd_draw_image(
         LOC_GAME_LABEL_FUNDS_X + 45,
-        LOC_GAME_LABEL_FUNDS_Y,
-        D->width,
-        D->height,
-        D->bitmap,
+        LOC_GAME_LABEL_FUNDS_Y - 10,
+        D.width,
+        D.height,
+        D.bitmap,
         fcolor,
-        D->bcolor,
+        D.bcolor,
         false);
 
         // Turn LCD On S
      lcd_draw_image(
         LOC_GAME_LABEL_FUNDS_X + 60,
-        LOC_GAME_LABEL_FUNDS_Y,
-        S->width,
-        S->height,
-        S->bitmap,
+        LOC_GAME_LABEL_FUNDS_Y - 10,
+        S.width,
+        S.height,
+        S.bitmap,
         fcolor,
-        S->bcolor,
+        S.bcolor,
         false);
 
         
         // Turn LCD On :
      lcd_draw_image(
         LOC_GAME_LABEL_FUNDS_X + 75,
-        LOC_GAME_LABEL_FUNDS_Y,
-        colon->width,
-        colon->height,
-        colon->bitmap,
+        LOC_GAME_LABEL_FUNDS_Y - 10,
+        colon.width,
+        colon.height,
+        colon.bitmap,
         fcolor,
-        colon->bcolor,
+        colon.bcolor,
         false);
 
-        free(F);
-        free(U);
-        free(N);
-        free(D);
-        free(S);
-        free(colon);
 
         /// the following code will be stat funds ///
         char str[6] = {0};
@@ -114,21 +108,29 @@ void screen_display_stats_funds(int16_t funds, uint16_t fcolor)
       
         for(int i = 0; str[i] != NULL; i++){
             // initialize number character pointer 
-            image_t *C = (image_t *)malloc(sizeof(image_t));
-            font_get_image(str[i], C);
+            image_t C;
+            font_get_image(str[i], &C);
+
+             lcd_draw_rectangle(
+                LOC_GAME_STATS_FUNDS_X - 4 +(i*20), // x start
+                C.width + 8,
+                LOC_GAME_STATS_FUNDS_Y - 10 - 4, // y start
+                C.height + 8,
+                LCD_COLOR_BLACK,
+                false);
 
             // draw the character
             lcd_draw_image(
-                LOC_GAME_STATS_FUNDS_X + (i*15),
-                LOC_GAME_STATS_FUNDS_Y,
-                C->width,
-                C->height,
-                C->bitmap,
+                LOC_GAME_STATS_FUNDS_X + (i*20),
+                LOC_GAME_STATS_FUNDS_Y - 10,
+                C.width,
+                C.height,
+                C.bitmap,
                 fcolor,
-                C->bcolor,
+                C.bcolor,
                 false);
 
-            free(C);
+          
         }
 }
 
@@ -141,65 +143,62 @@ void screen_display_stats_funds(int16_t funds, uint16_t fcolor)
 void screen_display_stats_bet(int16_t bet, uint16_t fcolor)
 {
     // initialize all associate character
-    image_t *B = (image_t *)malloc(sizeof(image_t));
-    image_t *E = (image_t *)malloc(sizeof(image_t));
-    image_t *T = (image_t *)malloc(sizeof(image_t));
-    image_t *colon = (image_t *)malloc(sizeof(image_t));
+    image_t B; 
+    image_t E;
+    image_t T;
+    image_t colon;
 
     // get character image
-    font_get_image('B', B);
-    font_get_image('E', E);
-    font_get_image('T', T);
-    font_get_image(':', colon);
+    font_get_image('B', &B);
+    font_get_image('E', &E);
+    font_get_image('T', &T);
+    font_get_image(':', &colon);
 
      // Turn LCD On B
      lcd_draw_image(
         LOC_GAME_LABEL_BET_X,
-        LOC_GAME_LABEL_BET_Y,
-        B->width,
-        B->height,
-        B->bitmap,
+        LOC_GAME_LABEL_BET_Y - 10,
+        B.width,
+        B.height,
+        B.bitmap,
         fcolor,
-        B->bcolor,
+        B.bcolor,
         false);
 
          // Turn LCD On E
      lcd_draw_image(
         LOC_GAME_LABEL_BET_X + 15,
-        LOC_GAME_LABEL_BET_Y,
-        E->width,
-        E->height,
-        E->bitmap,
+        LOC_GAME_LABEL_BET_Y - 10,
+        E.width,
+        E.height,
+        E.bitmap,
         fcolor,
-        E->bcolor,
+        E.bcolor,
         false);
 
         // Turn LCD On T
      lcd_draw_image(
         LOC_GAME_LABEL_BET_X + 30,
-        LOC_GAME_LABEL_BET_Y,
-        T->width,
-        T->height,
-        T->bitmap,
+        LOC_GAME_LABEL_BET_Y - 10,
+        T.width,
+        T.height,
+        T.bitmap,
         fcolor,
-        T->bcolor,
+        T.bcolor,
         false);
 
          // Turn LCD On colon
      lcd_draw_image(
         LOC_GAME_LABEL_BET_X + 45,
-        LOC_GAME_LABEL_BET_Y,
-        colon->width,
-        colon->height,
-        colon->bitmap,
+        LOC_GAME_LABEL_BET_Y - 10,
+        colon.width,
+        colon.height,
+        colon.bitmap,
         fcolor,
-        colon->bcolor,
+        colon.bcolor,
         false);
 
-        free(B);
-        free(E);
-        free(T);
-        free(colon);
+   
 
         /// the following code will be bet amount ///
         char str[6] = {0};
@@ -207,21 +206,30 @@ void screen_display_stats_bet(int16_t bet, uint16_t fcolor)
       
         for(int i = 0; str[i] != NULL; i++){
             // initialize number character pointer 
-            image_t *C = (image_t *)malloc(sizeof(image_t));
-            font_get_image(str[i], C);
+            image_t C;
+            font_get_image(str[i], &C);
+
+            lcd_draw_rectangle(
+                LOC_GAME_STATS_BET_X - 4 +(i*20), // x start
+                C.width + 8,
+                LOC_GAME_STATS_BET_Y - 10 - 4, // y start
+                C.height + 8,
+                LCD_COLOR_BLACK,
+                false);
+
 
             // draw the character
             lcd_draw_image(
                 LOC_GAME_STATS_BET_X + (i*15),
-                LOC_GAME_STATS_BET_Y,
-                C->width,
-                C->height,
-                C->bitmap,
+                LOC_GAME_STATS_BET_Y - 10,
+                C.width,
+                C.height,
+                C.bitmap,
                 fcolor,
-                C->bcolor,
+                C.bcolor,
                 false);
 
-            free(C);
+            
         }
 }
 
@@ -239,21 +247,21 @@ void screen_display_stats_dealer_hand(int16_t hand_value, uint16_t fcolor)
    
      for(int i = 0; str[i] != NULL; i++){
          // initialize number character pointer 
-         image_t *C = (image_t *)malloc(sizeof(image_t));
-         font_get_image(str[i], C);
+         image_t C;
+         font_get_image(str[i], &C);
 
          // draw the character
          lcd_draw_image(
             LOC_GAME_STATS_DEALER_HAND_X + (i*15),
-            LOC_GAME_STATS_DEALER_HAND_Y,
-             C->width,
-             C->height,
-             C->bitmap,
+            LOC_GAME_STATS_DEALER_HAND_Y - 10,
+             C.width,
+             C.height,
+             C.bitmap,
              fcolor,
-             C->bcolor,
+             C.bcolor,
              false);
 
-         free(C);
+         
      }
 }
 
@@ -271,20 +279,20 @@ void screen_display_stats_player_hand(int16_t hand_value, uint16_t fcolor)
   
     for(int i = 0; str[i] != NULL; i++){
         // initialize number character pointer 
-        image_t *C = (image_t *)malloc(sizeof(image_t));
-        font_get_image(str[i], C);
+        image_t C;
+        font_get_image(str[i], &C);
 
         // draw the character
         lcd_draw_image(
            LOC_GAME_STATS_PLAYER_HAND_X + (i*15),
-           LOC_GAME_STATS_PLAYER_HAND_Y,
-            C->width,
-            C->height,
-            C->bitmap,
+           LOC_GAME_STATS_PLAYER_HAND_Y - 10,
+            C.width,
+            C.height,
+            C.bitmap,
             fcolor,
-            C->bcolor,
+            C.bcolor,
             false);
 
-        free(C);
+       
     }
 }
